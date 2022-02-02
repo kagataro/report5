be5e10ad1fa1e9397f8ebf258645caf0e9713f7a
kagataro <e215758@ie.u-ryukyu.ac.jp>
2022-02-02 21:43:20 +0900

    Add Main method

---
+++ app/src/main/java/jp/ac/uryukyu/Main.java
@@ -0,0 +1,8 @@
+import jp.ac.uryukyu.ie.e215758.*;
+
+public class Main{
+    public static void main(String[] args){
+        String str = null;
+        System.out.println(str.length());
+    }
+}
